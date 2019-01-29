80144f58d3a7cf6e24f9a76634d1f2323a0e14a1
Ernest <emuroiwa@gmail.com>
2019-01-29 18:37:01 +0200

    creating route

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('{path}',"HomeController@index")->where( 'path', '([A-z\d-\/_.]+)?' );
 
+Route::apiResources(['payment' => 'API\PaymentController']);
